2d09b6804207e37621aebbe9f34f0d2c347a291e
ZhuoQiuMcgill <81338304+ZhuoQiuMcgill@users.noreply.github.com>
2025-08-07 20:49:35 -0400

    垃圾warp

--- frontend/src/components/index.js
+++
@@ -1,5 +0,0 @@
-// Export all components from a single entry point
-export { default as NavHeader } from './NavHeader'
-export { default as MeshCanvas } from './MeshCanvas'
-export { default as TrainingMonitor } from './TrainingMonitor'
-export { default as Breadcrumb } from './Breadcrumb'
--- frontend/src/components/ui/index.js
+++
@@ -1,8 +0,0 @@
-// Export all UI components from a single entry point
-export { default as Button } from './Button'
-export { default as PanelCard } from './PanelCard'
-export { default as CompactStatusBar } from './CompactStatusBar'
-export { default as FormInput } from './FormInput'
-export { default as FormSelect } from './FormSelect'
-export { default as LoadingOverlay } from './LoadingOverlay'
-export { default as EmptyState } from './EmptyState'
